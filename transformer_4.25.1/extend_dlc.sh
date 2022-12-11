de18196e2b3226249df89a827203f89c46f89f5c
Xinhao Wang <xinhao@sunshine.com>
2022-12-10 18:32:57 -0800

    transformer_4.25.1

---
+++ transformer_4.25.1/extend_dlc.sh
@@ -0,0 +1,31 @@
+%%sh
+
+# Specify a name and a tag
+algorithm_name=huggingface-pytorch-extended
+tag=1.10.2-transformers4.25.1-gpu-py38-cu113-ubuntu20.04
+
+account=$(aws sts get-caller-identity --query Account --output text)
+
+# Get the region defined in the current configuration (default to us-west-2 if none defined)
+region=$(aws configure get region)
+
+fullname="${account}.dkr.ecr.${region}.amazonaws.com/${algorithm_name}:${tag}"
+
+# If the repository doesn't exist in ECR, create it.
+
+aws ecr describe-repositories --repository-names "${algorithm_name}" > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+aws ecr create-repository --repository-name "${algorithm_name}" > /dev/null
+fi
+
+# Log into Docker
+aws ecr get-login-password --region ${region}|docker login --username AWS --password-stdin ${fullname}
+
+# Build the docker image locally with the image name and then push it to ECR
+# with the full name.
+
+docker build -t ${algorithm_name} .
+docker tag ${algorithm_name} ${fullname}
+
+docker push ${fullname}
